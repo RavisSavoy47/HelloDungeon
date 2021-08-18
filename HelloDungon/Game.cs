a0b05f0f3ac5779f11d7ca91904d8c91c22dec2d
ravis <ravissavoy49@gmail.com>
2021-08-18 09:57:48 -0500

    Added test commit comment

--- HelloDungon/Game.cs
+++ HelloDungon/Game.cs
@@ -8,44 +8,22 @@ namespace HelloDungeon
     { 
         public void Run()
         {
-            //Health exercise
-
-            //Intitalize default health values
-            int healthRegen = 50;
-            int health = 100 / 2;
-            //Increase health by regen amount
-            health = health + healthRegen;
-            health += healthRegen;
-            //Display health to user
-            Console.WriteLine(health);
-
             //Name exercise
             string name = "Empty";
             Console.WriteLine("Please enter your name");
             name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
-
             string characterName = (name);
-            int currectArea = 1;
-            float damage = 10;
-            float speed = 5;
-            float range = 5;
-           
-            bool gameOver = false;
-            //Mana 
-            int mana = 100;
-            int manaRegen = 20;
-            mana += manaRegen;
-            Console.WriteLine(mana);
+          
             //introduction
             Console.WriteLine("Weclome to Waistlands");
             Console.WriteLine("This is a place ware you forge your own path");
             //class selection
             Console.WriteLine("What class would you like to choose");
-            Console.Write("Choose between. Gunslinger or Brute");
+            Console.WriteLine("Choose between. Gunslinger or Brute");
             string characterClass = Console.ReadLine();
-            int Gunslinger;
-            int Brute;
+           //test
+          
 
 
 
--- HelloDungon/Program.cs
+++ HelloDungon/Program.cs
@@ -9,6 +9,27 @@ namespace HelloDungeon
             //Create a new instance of Game
             Game game = new Game();
 
+            //Intitalize default health values
+            int healthRegen = 50;
+            int health = 100 / 2;
+            //Increase health by regen amount
+            health += healthRegen;
+            Console.WriteLine(health);
+
+            int currectArea = 1;
+            float damage = 10;
+            float speed = 5;
+            float range = 5;
+
+            bool gameOver = false;
+            //Mana 
+            int mana = 100 / 2;
+            int manaRegen = 50;
+            mana += manaRegen;
+            //Character Classes
+            int Gunslinger;
+            int Brute;
+            
             //Run the Game
             game.Run();
         }
